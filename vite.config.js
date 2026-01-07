af5eb415bf8058ff0649e5a5433fef57f0f69f9b
Selomitha Nong <nongselomitha@gmail.com>
2026-01-08 01:35:45 +0800

    fix: STUDENT.txt, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/story-app/',
+  base: '/storyApp/',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src/public'),
 
